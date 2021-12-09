2c360b8a61449310853022f4537d6593b35e1d80
Johanu GANDONOU <johanugandiniu@gmail.com>
2021-12-09 02:18:49 +0100

    init

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :recoverable, :trackable, :registerable and :omniauthable
-  devise :database_authenticatable, :rememberable, :validatable
+  # :confirmable, :lockable, :timeoutable, :trackable, :registerable and :omniauthable
+  devise :database_authenticatable, :rememberable, :validatable, :recoverable
 end
